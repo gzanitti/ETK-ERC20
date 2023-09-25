0983cd65f8acdf915dc939af721fda26e40aad05
Gaston Zanitti <gzanitti@gmail.com>
2023-09-25 11:57:17 -0300

    WIP: ERC20 basic implementation

---
+++ script/compile.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+echo "0x$(/home/gzanitti/EVOSSD/EPF4/etk/target/debug/eas $1)"
---
+++ test/Contract.t.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+contract ContractTest is Test {
+    address golf;
+    address user;
+
+    function _loadCode(address addr) internal {
+        string[] memory inputs = new string[](2);
+        /**
+         * windows: script/compile.bat
+         * linux  : script/compile.sh
+         */
+        inputs[0] = "script/compile.sh";
+
+        // path/to/contract.etk
+        inputs[1] = "src/Contract.etk";
+
+        bytes memory bytecode = vm.ffi(inputs);
+
+        emit logs(bytecode);
+
+        vm.etch(addr, bytecode);
+    }
+
+    function setUp() public {
+        golf = address(0x9014);
+        user = address(0x900d);
+
+        vm.label(golf, "Golf");
+        vm.label(user, "User");
+
+        _loadCode(golf);
+    }
+
+    function testExample() public {
+        assertTrue(true);
+    }
+}
